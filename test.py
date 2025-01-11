610502c53f9bdbf46278f81635fe92a7338e2c3e
Gaurav-Kaushal <gauravkaushal1996@gmail.com>
2025-01-11 20:43:51 +0530

    Update

--- test.py
+++ test.py
@@ -7,27 +7,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
 
-# XPaths for input fields
 inputName = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input'
 inputEmailID = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input'
 inputPhone = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
 
-# Submit Button Xpath
 Submit = '/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div/div'
 
 def sleep(duration=3):
     time.sleep(duration)
 
-# Prompt the user for the form URL
 form_url = input("Please enter the Google Form URL: ")
 
-# Initialize the browser with WebDriver Manager
 browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
-# Open the provided Google Form
 browser.get(form_url)
 
-# Read data from CSV
 name = []
 email = []
 phone = []
@@ -39,10 +33,8 @@ with open("input.csv", "r") as f_input:
         email.append(row['email'])
         phone.append(row['phone_number'])
 
-# Automate form submission
 for i in range(len(name)):
     try:
-        # Wait for input fields to be visible and send data
         WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, inputName)))
         browser.find_element(By.XPATH, inputName).send_keys(name[i])
 
@@ -54,18 +46,15 @@ for i in range(len(name)):
 
         sleep()
 
-        # Click submit
         WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, Submit)))
         browser.find_element(By.XPATH, Submit).click()
 
         sleep()
 
-        # Go back to the form for next entry
         browser.back()
         sleep()
 
     except Exception as e:
         print(f"Error at index {i}: {e}")
 
-# Close the browser
 browser.quit()
